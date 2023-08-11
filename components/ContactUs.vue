128b82ccbb4e02f8aac547f06e51f37d00d1df67
mohamedkorenymohamed <mohamed.koreny.mohamed@gmail.com>
2023-08-11 06:34:40 -0700

    feat: contact us ui progress

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -1,3 +1,122 @@
 <template>
-    hello from contact us 
+                                        <!--  Contact Us  -->
+  <section class="py-4 bg-primary contact-us">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-7 col-sm-12">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="title py-5 d-flex flex-column align-content-start">
+                <p class="text-uppercase">get in touch</p>
+                <p class="text-uppercase">with <span>digify</span></p>
+                <p class="text-uppercase">team today <span>!</span></p>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <form>
+                <div class="contact-info d-flex flex-column align-items-start">
+                  <div class="input-name">
+                    <input type="text" placeholder="Your Name" />
+                  </div>
+                  <div class="input-email">
+                    <input type="email" placeholder="Your Email" />
+                  </div>
+                  <div class="input-subject">
+                    <input type="text" placeholder=" Subject" />
+                  </div>
+                  <div class="input-message">
+                    <input type="text" placeholder="Your Message (Optional)" />
+                  </div>
+                  <button class="btn btn-secondary rounded-pill fs-6">s</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+                                      <!--  Saudi Branch Ifo  -->
+  <section class="saudi-branch bg-primary py-5">
+    <div class="container">
+      <div class="row gy-5">
+        <div class="col-md-7 ">
+            <img src="../assets/image/fox.png" class="w-100 img-fluid" alt="fox-image">
+        </div>
+        <div class="col-md-5 d-flex align-items-center">
+            <div class="branch-details ">
+                <h1  >saudi <span >arabia</span> branch</h1>  
+                <p>office #101 the <br> courtyard, prince saud al faisal sT.jeddah</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+                                      <!--  Cairo Branch Ifo  -->
+  <section class="cairo-branch bg-primary py-5">
+    <div class="container">
+      <div class="row gy-5">
+        <div class="col-md-7 ">
+            <img src="../assets/image/Lama.png" class="w-100 img-fluid" alt="fox-image">
+        </div>
+        <div class="col-md-5 d-flex align-items-center">
+            <div class="branch-details ">
+              <h1  > <span >cairo</span> branch</h1>  
+                <p>82 abdulaziz fahmy cairo, cairo <br> governorate, egypt 11361 </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+                                          <!--  Email Us  -->
+  <section class="email-us bg-primary py-5">
+    <div class="container">
+      <div class="row gy-5">
+        <div class="col-md-7 ">
+            <img src="../assets/image/Lama.png" class="w-100 img-fluid" alt="fox-image">
+        </div>
+        <div class="col-md-5 d-flex align-items-center">
+            <div class="email-details ">
+              <h1  > email<span >us</span> </h1>  
+                <p>info@digifymena.com </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+                                          <!--  Ring Us  -->
+  <section class="ring-us bg-primary py-5">
+    <div class="container">
+      <div class="row gy-5">
+        <div class="col-md-7 ">
+            <img src="../assets/image/ring.png" class="w-100 img-fluid" alt="fox-image">
+        </div>
+        <div class="col-md-5 d-flex align-items-center">
+            <div class="ring-details ">
+                <h1  >ring <span>us</span></h1>  
+                <p>+20227748700 <br> +201144447609 </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+                                      <!--  Ring Us Rectangle -->
+  <section class="ring-us bg-primary py-5">
+    <div class="container">
+      <div class="row gy-5">
+        <div class="col-md-7 ">
+            <img src="../assets/image/Rectangle 40503.png" class="w-100 img-fluid" alt="fox-image">
+        </div>
+        <div class="col-md-5 d-flex align-items-center">
+            <div class="ring-details ">
+                <h1  >ring <span>us</span></h1>  
+                <p>+20227748700 <br> +201144447609 </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
         <Banner />
         <about-us />
+        <ContactUs /> 
 </template>
 
 <style lang="scss"  >
