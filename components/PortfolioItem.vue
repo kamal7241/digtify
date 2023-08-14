d28204e425a5f1cc9b72ae28b454dec8f4bfd532
kamal7241 <kamal.korney@gmail.com>
2023-08-15 00:49:22 +0300

    feat:portfolio - init animations progress

---
+++ components/PortfolioItem.vue
@@ -0,0 +1,15 @@
+<template>
+                <div class="portfolio-item" >
+                    <div class="portfolio-item__img" >
+                        <slot ></slot>
+                    </div>
+                    <h3 class="portfolio-item__title">{{title}}</h3>
+                </div>
+
+</template>
+
+<script>
+export default defineNuxtComponent({
+    props:['title']
+})
+</script>
