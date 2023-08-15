8a0fb09d6fe2dd20a8a6da1760af6ecd71a74286
mohamedkorenymohamed <mohamed.koreny.mohamed@gmail.com>
2023-08-14 17:25:07 -0700

    feat:contact us

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -1,31 +1,65 @@
 <template>
-    <scroll-animator @next="currentProgress++" @prev="currentProgress--" :breakPointsNumber="9" :gap="500" :startThresHold="100" >
-        <CoreHomeSectionContainer  class="home-sec--portfolio pt-4" >
-            <div class="animate__animated "
-                :class="{ 'animate__fadeOutLeftBig' :  currentProgress >= PROGRESS.titleMoveUpAndFade}">
-            <div class="home-sec--portfolio__title-wrapper" >
-                    <core-zoomed-title 
-                    origin="58px 100px" 
-                    class="home-sec__title"  
-                        :scale="currentProgress == PROGRESS.titleZoomIn ? 50 : 1"
-                    >
-                                <span class="text-white" >CONTACT</span>
-                    </core-zoomed-title>
-    <h2 class="text-secondary home-sec__title" >US</h2>
-            </div>
+  <scroll-animator
+    @next="currentProgress++"
+    @prev="currentProgress--"
+    :breakPointsNumber="9"
+    :gap="500"
+    :startThresHold="100"
+  >
+    <CoreHomeSectionContainer class="home-sec--portfolio pt-4">
+      <div
+        class="animate__animated"
+        :class="{
+          animate__fadeOutLeftBig:
+            currentProgress >= PROGRESS.titleMoveUpAndFade,
+        }"
+      >
+        <div class="home-sec--portfolio__title-wrapper">
+          <core-zoomed-title
+            origin="58px 100px"
+            class="home-sec__title"
+            :scale="currentProgress == PROGRESS.titleZoomIn ? 50 : 1"
+          >
+            <span class="text-white">CONTACT</span>
+          </core-zoomed-title>
+          <h2 class="text-secondary home-sec__title">US</h2>
         </div>
-            
-            <transition-group mode="out-in"  name="fadeInDelayOutLeft" >
-                <core-card class="animate__animated" :key="1" v-if="currentProgress == PROGRESS.firstSlide" />
-                <core-card class="bg-primary animate__animated" :key="2" v-else-if="currentProgress == PROGRESS.secondSlide" />
-                <core-card class="animate__animated bg-info" :key="3" v-else-if="currentProgress == PROGRESS.thirdSlide" />
-                <core-card class="animate__animated bg-danger" :key="4" v-else-if="currentProgress == PROGRESS.fourthSlide" />
-                <core-card class="animate__animated bg-secondary" :key="5" v-else-if="currentProgress == PROGRESS.fifthSlide" />
-                <core-card class="animate__animated bg-success" :key="6" v-else-if="currentProgress == PROGRESS.sixthSlide" />
-            </transition-group>
-              
-        </CoreHomeSectionContainer>
-    </scroll-animator>
+      </div>
+
+      <transition-group mode="out-in" name="fadeInDelayOutLeft">
+        <core-card
+          class="animate__animated"
+          :key="1"
+          v-if="currentProgress == PROGRESS.firstSlide"
+        />
+        <core-card
+          class="bg-primary animate__animated"
+          :key="2"
+          v-else-if="currentProgress == PROGRESS.secondSlide"
+        />
+        <core-card
+          class="animate__animated bg-info"
+          :key="3"
+          v-else-if="currentProgress == PROGRESS.thirdSlide"
+        />
+        <core-card
+          class="animate__animated bg-danger"
+          :key="4"
+          v-else-if="currentProgress == PROGRESS.fourthSlide"
+        />
+        <core-card
+          class="animate__animated bg-secondary"
+          :key="5"
+          v-else-if="currentProgress == PROGRESS.fifthSlide"
+        />
+        <core-card
+          class="animate__animated bg-success"
+          :key="6"
+          v-else-if="currentProgress == PROGRESS.sixthSlide"
+        />
+      </transition-group>
+    </CoreHomeSectionContainer>
+  </scroll-animator>
   <!--  Contact Us  -->
   <section class="py-4 bg-primary contact-us">
     <div class="container">
@@ -45,16 +79,39 @@
               <form>
                 <div class="contact-info d-flex flex-column align-items-start">
                   <div class="input-name">
-                    <input type="text" placeholder="Your Name" />
+                    <input
+                      type="text"
+                      v-model="state.name"
+                      placeholder="Your Name"
+                    />
+                    <div
+                      class="input-errors"
+                      v-for="error of v$.name.$errors"
+                      :key="error.$uid"
+                    >
+                      <div class="error-msg">{{ error.$message }}</div>
+                    </div>
                   </div>
                   <div class="input-email">
-                    <input type="email" placeholder="Your Email" />
+                    <input
+                      type="email"
+                      v-model="state.email"
+                      placeholder="Your Email"
+                    />
                   </div>
                   <div class="input-subject">
-                    <input type="text" placeholder=" Subject" />
+                    <input
+                      type="text"
+                      v-model="state.subject"
+                      placeholder=" Subject"
+                    />
                   </div>
                   <div class="input-message">
-                    <input type="text" placeholder="Your Message (Optional)" />
+                    <input
+                      type="text"
+                      v-model="state.message"
+                      placeholder="Your Message (Optional)"
+                    />
                   </div>
                   <button class="btn btn-secondary rounded-pill fs-6">s</button>
                 </div>
@@ -180,62 +237,76 @@
 </template>
 
 <script>
+import Vuelidate from "vuelidate";
+import { required, email } from "vuelidate/lib/validators";
+
 const PROGRESS = Object.freeze({
-    titleZoomIn : 0 , 
-    titleZoomOut : 1 , 
-    titleMoveUpAndFade : 2 , 
-    firstSlide : 3 , 
-    secondSlide : 4  ,
-    thirdSlide : 5  ,
-    fourthSlide : 6  ,
-    fifthSlide : 7  ,
-    sixthSlide : 8  ,
-}); 
+  titleZoomIn: 0,
+  titleZoomOut: 1,
+  titleMoveUpAndFade: 2,
+  firstSlide: 3,
+  secondSlide: 4,
+  thirdSlide: 5,
+  fourthSlide: 6,
+  fifthSlide: 7,
+  sixthSlide: 8,
+});
 const PROGRESS_HIGHLIGHT_RATIO = Object.freeze({
-    [PROGRESS.titleZoomIn] : 100 , 
-    [PROGRESS.titleZoomOut] : 100 , 
-    [PROGRESS.titleMoveUpAndFade] : 0 , 
-    [PROGRESS.firstSlide] : 15 ,
-    [PROGRESS.secondSlide] : 30 ,  
-    [PROGRESS.thirdSlide] : 45 ,  
-    [PROGRESS.fourthSlide] : 60 ,  
-    [PROGRESS.fifthSlide] : 75 ,  
-    [PROGRESS.sixthSlide] : 100 ,  
-})
+  [PROGRESS.titleZoomIn]: 100,
+  [PROGRESS.titleZoomOut]: 100,
+  [PROGRESS.titleMoveUpAndFade]: 0,
+  [PROGRESS.firstSlide]: 15,
+  [PROGRESS.secondSlide]: 30,
+  [PROGRESS.thirdSlide]: 45,
+  [PROGRESS.fourthSlide]: 60,
+  [PROGRESS.fifthSlide]: 75,
+  [PROGRESS.sixthSlide]: 100,
+});
 const PROGRESS_TYPES = Object.freeze({
   forward: 1,
   backward: 2,
 });
 export default defineNuxtComponent({
-    data(){
-        return {
-            currentProgress : PROGRESS.titleZoomIn ,
-            progressType: PROGRESS_TYPES.forward
-        }
-    } ,
-    computed:{
-        PROGRESS(){
-            return PROGRESS;
-        } ,
-        PROGRESS_TYPES(){
-            return PROGRESS_TYPES;
-        },
-        PROGRESS_HIGHLIGHT_RATIO(){
-            return PROGRESS_HIGHLIGHT_RATIO;
-        } , 
-        // getUrl(){
-        //     return require('../assets/image/404Error.jpg')
-        // } , 
-        getUrl() {
-            return new URL(`../assets/image/404Error.jpg`, import.meta.url).href
-        }
+  data() {
+    return {
+      currentProgress: PROGRESS.titleZoomIn,
+      progressType: PROGRESS_TYPES.forward,
+      state: {
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      },
+    };
+  },
+  validations: {
+    yourName: { required }, // Matches this.firstName
+    email: { required }, // Matches this.lastName
+    subject: { required },
+    message: { required },
+  },
+  computed: {
+    PROGRESS() {
+      return PROGRESS;
     },
-    watch:{
-        currentProgress(curr, prev) {
-          this.progressType =
-            curr > prev ? PROGRESS_TYPES.forward : PROGRESS_TYPES.backward;
-        },
-    }
-})
-
+    PROGRESS_TYPES() {
+      return PROGRESS_TYPES;
+    },
+    PROGRESS_HIGHLIGHT_RATIO() {
+      return PROGRESS_HIGHLIGHT_RATIO;
+    },
+    // getUrl(){
+    //     return require('../assets/image/404Error.jpg')
+    // } ,
+    getUrl() {
+      return new URL(`../assets/image/404Error.jpg`, import.meta.url).href;
+    },
+  },
+  watch: {
+    currentProgress(curr, prev) {
+      this.progressType =
+        curr > prev ? PROGRESS_TYPES.forward : PROGRESS_TYPES.backward;
+    },
+  },
+});
 </script>
