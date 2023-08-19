da5e58848ad7ef34c78a3f30b8f8e7dd39cf16f9
kamal7241 <kamal.korney@gmail.com>
2023-08-19 19:01:28 +0300

    feat:config - init build config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const netilfyConfig = {
+  baseURL: '/',
+  buildAssetsDir: 'assets'
+}
+const ghPagesConfig = {
+    baseURL: '/digtify/',
+    buildAssetsDir: 'assets'
+}
+
+const config = process.env.NUXT_ENV == 'gh' ? ghPagesConfig : 
+process.env.NUXT_ENV == 'netilfy' ? netilfyConfig : {}
 export default defineNuxtConfig({
+  app: {
+    ...config
+  },
   devtools: { enabled: true },
   css: [
     // '@/assets/sass/main-ltr.scss',
