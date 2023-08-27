b752fccd1b5dfdeaa6d82f95e24caeb352178f5c
mohamedkorenymohamed <mohamed.koreny.mohamed@gmail.com>
2023-08-26 23:58:35 -0700

    feat - add social items

---
+++ components/SocialLinks.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="social-links" >
+    <div class="dropdown  ">
+        <div id="menu">
+  <button @click="dropIt" class="btn-dropdown  rounded-pill">Pet </button>
+  <transition name="slide">
+    <ul class=" list" v-if="isDropped">
+      <li class="drop-item rounded-pill"><a href=""><img src= '../assets/image/facebook.svg'    ></a></li>
+      <li class="drop-item rounded-pill"><a href=""> <img src="../assets/image/linkedin.svg" ></a></li>
+      <li class="drop-item rounded-pill"><a href=""> <img src="../assets/image/youtube.svg"> </a></li>
+    </ul>
+  </transition>
+  </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    el: '#menu',
+  data: () => ({
+    isDropped: false
+  }),
+  methods: {
+    dropIt() {
+      this.isDropped = !this.isDropped
+    }
+  }
+
+}
+</script>
+
+
+
