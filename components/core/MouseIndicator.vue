c916163c633556e4fa0cb0b3512495ba9bb0dde4
kamal7241 <kamal.korney@gmail.com>
2023-09-03 01:18:44 +0300

    feat:indicators - add mouse indicator

---
+++ components/core/MouseIndicator.vue
@@ -0,0 +1,39 @@
+<template >
+    <Transition>
+        <div class="mouse-indicator " @wheel="eject" v-show="show" >
+            <div class="mouse">
+                <span class="mouse-wheel"></span>
+            </div>
+            <h1 class="postion-relative text-white mt-3" > Explore Digify using your mouse or trackpad </h1>
+        </div>
+
+    </Transition>
+</template>
+
+<script>
+export default defineNuxtComponent({
+    data() {
+        return {
+            show: false , 
+        }
+    },
+    methods:{
+        checkIsFirstLoad(){
+            const preferences = JSON.parse(localStorage.getItem("preferences"));
+            this.show = !preferences;
+        } ,
+        eject(){
+            if(this.show){
+                this.show = false
+                localStorage.setItem("preferences" , true)
+            }
+        }
+
+
+    },
+    mounted(){
+        this.checkIsFirstLoad()
+    }
+})
+
+</script>
