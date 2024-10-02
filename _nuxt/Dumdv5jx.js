48e60384315afc9f8fcdf77413ee74f6318ca9cb
kkhalil <kkhalil@moj.gov.sa>
2024-10-02 10:23:08 +0300

    Updates

---
+++ _nuxt/Dumdv5jx.js
@@ -0,0 +1,1 @@
+const e={services:{title:{t:0,b:{t:2,i:[{t:3}],s:"Our Services"}},description:{t:0,b:{t:2,i:[{t:3}],s:"We offer various services."}}}};export{e as default};
--- assets/home.b6eaecaf.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./entry.d3a3c052.js";const o={};function r(c,t){return" hello from home "}const n=e(o,[["render",r]]);export{n as default};
