cdae98507e48bd2e142e8e3a5e9cca92ba3ae290
kkhalil <kkhalil@moj.gov.sa>
2024-10-02 11:05:28 +0300

    Updates

---
+++ assets/CC3nftoU.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./CUlyaV0E.js";const r={};function c(n,t){return" hello "}const o=e(r,[["render",c]]);export{o as default};
--- assets/Dhtm9GBo.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./CtIe1NhU.js";const r={};function c(n,t){return" hello "}const o=e(r,[["render",c]]);export{o as default};
