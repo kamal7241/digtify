8f516ae775b7997f605c9a234a11e59336f1cb1f
Mohamed Korney Mohamed <123994927+mohamedkorneymohamed@users.noreply.github.com>
2024-10-06 06:40:26 -0700

    fix-ui-saudi-digital-campaign-page

--- components/OurServices.vue
+++ components/OurServices.vue
@@ -1,53 +1,59 @@
 <template>
-  <div class="our-services ">
-    <v-container   >
+  <div class="our-services">
+    <v-container>
       <div
         class="our-services__header d-flex justify-content-between items-center"
       >
-      <NuxtLink to="/en" >
-        <img
-          class="logo"
-          alt="digify-logo"
-          src="@/assets/image/digify-logo.png"
-        />
-      </NuxtLink>
-      <div 
-      :class="[
-      'd-flex flex-nowrap align-items-center gap-3',
-      lang === 'ar' ? 'flex-row-reverse' : 'flex-row',
-      ]" 
-      >
-          <span 
-          class="text-sm text-white fw-bold"
-          :class="lang === 'en' ? '' : 'opacity-50'"
-          
-          >EN</span>
-          <v-switch :modelValue="lang" :hide-details="true" 
-           false-value="en"
+        <NuxtLink to="/en">
+          <img
+            class="logo"
+            alt="digify-logo"
+            src="@/assets/image/digify-logo.png"
+          />
+        </NuxtLink>
+        <div
+          :class="[
+            'd-flex flex-nowrap align-items-center gap-3',
+            lang === 'ar' ? 'flex-row-reverse' : 'flex-row',
+          ]"
+        >
+          <span
+            class="text-sm text-white fw-bold"
+            :class="lang === 'en' ? '' : 'opacity-50'"
+            >EN</span
+          >
+          <v-switch
+            :modelValue="lang"
+            :hide-details="true"
+            false-value="en"
             true-value="ar"
             class="switch-style"
-          @click="switchLanguage"
+            @click="switchLanguage"
           >
           </v-switch>
-          <span class="text-sm text-white fw-bold"
-          :class="lang === 'ar' ? '' : 'opacity-50'"
-          >ع</span>
-     
-
-      </div>
+          <span
+            class="text-sm text-white fw-bold"
+            :class="lang === 'ar' ? '' : 'opacity-50'"
+            >ع</span
+          >
+        </div>
       </div>
     </v-container>
-    <v-container class="mb-6">
-      <div class="our-services__title-and-subtitle">
-        <h1 class="subtitle mt-3 mb-3">
-          {{ $t('services.description') }}
-        </h1>
+    <v-container class="mb-6 animate__animated">
+      <div class="our-services__intro animate__animated animate__fadeInDown">
+        <h4 class="subtitle mt-3 mb-3">
+          {{ $t("services.intro") }}
+        </h4>
       </div>
-      <v-row
-      >
-        <v-col cols="12" md="6">
+      <v-row class="animate__animated animate__fadeInUpBig">
+        <v-col cols="12" md="6" class="order-2 order-md-1">
           <v-sheet class="bg-transparent p-0 m-0">
-            <div class="our-services-content">
+            <div class="our-services-content ">
+              <div class="our-services__title-and-subtitle">
+                <h1 class="subtitle mt-3 mb-3">
+                  {{ $t("services.description") }}
+                </h1>
+              </div>
               <div class="our-services__desc">
                 <div class="mb-4" v-for="service in services" :key="service.id">
                   <h5 class="text-secondary">{{ service.title }}</h5>
@@ -55,14 +61,13 @@
                     {{ service.description }}
                   </p>
                   <p>
-                    <NuxtLink 
-                      class="d-inline-flex text-white text-sm gap-2 fs-6 mt-2" 
-                      :to="`https://wa.me/${phoneNumber}`"
-
-                      >
-                      <span class="" >{{ $t('contactForMoreDetails') }}</span>
-                        <v-icon class="text-xs" >mdi-whatsapp</v-icon>
-                  </NuxtLink>
+                    <NuxtLink
+                      class="d-inline-flex text-white text-sm gap-2 fs-6 mt-2"
+                      @click="openWhatsApp"
+                    >
+                      <span class="">{{ $t("contactForMoreDetails") }}</span>
+                      <v-icon class="text-xs">mdi-whatsapp</v-icon>
+                    </NuxtLink>
                   </p>
                 </div>
               </div>
@@ -74,35 +79,44 @@
             </div>
           </v-sheet>
         </v-col>
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="6" class="order-1 order-md-2">
           <img
-          class="mw-100"
-          alt="digify-logo"
-          src="@/assets/image/our-services/landing-page.png"
-        />
-      
-          <div class="d-flex justify-center gap-3 mt-4">
+            class="mw-100"
+            alt="digify-logo"
+            src="@/assets/image/our-services/landing-page.png"
+          />
+
+          <div
+            class="our-services__banner-btn d-flex justify-center gap-3 mt-4 items-center"
+          >
             <!-- <h1>{{ $t('welcome') }}</h1>
         <p>{{ $t('our-services.description') }}</p> -->
             <!-- <button @click="changeLanguage('ar')">Switch to Arabic</button> -->
             <NuxtLink to="/en">
-              <button class="btn btn-secondary text-uppercase fs-5 rounded-5 px-5">
-                {{$t('visitOurWebsite')}} 
+              <button
+                :class="`btn btn-secondary text-uppercase rounded-5 px-sm-5     h-100 ${
+                  lang === 'ar' ? ' px-4 ' : 'px-2 '
+                }`"
+              >
+                {{ $t("visitOurWebsite") }}
               </button>
             </NuxtLink>
-            <a 
-            :href="`${baseURL}/digify-portfolio.pdf`"
-            target="_blank"
-            download="digify-portfolio.pdf"
+            <a
+              :href="`${baseURL}/digify-portfolio.pdf`"
+              target="_blank"
+              download="digify-portfolio.pdf"
             >
-              <button class="btn btn-success text-uppercase fs-5 rounded-5 px-5">
-                {{$t('downloadPortfolio')}} 
-                <v-icon class="text-xs" >mdi-arrow-down-thin</v-icon>
+              <button
+                :class="`btn btn-success text-uppercase rounded-5 px-sm-5    ${
+                  lang === 'ar' ? ' px-4' : 'px-2 '
+                }`"
+              >
+                {{ $t("downloadPortfolio") }}
+                <v-icon class="text-xs">mdi-arrow-down-thin</v-icon>
               </button>
             </a>
           </div>
         </v-col>
-     
       </v-row>
     </v-container>
   </div>
@@ -111,32 +125,31 @@
 <script>
 import ENServices from "@/static/en/services.json";
 import ARServices from "@/static/ar/services.json";
-const phoneNumber = "+966 56 471 5121";
+const phoneNumber = "+966564715121";
 const message = "";
-
 
 export default {
   setup() {
     const config = useRuntimeConfig();
-    const baseURL = config.public.baseURL || ''; // Fallback to empty if baseURL is not defined
-    return{
+    const baseURL = config.public.baseURL || ""; // Fallback to empty if baseURL is not defined
+    return {
       baseURL,
-    }
+    };
   },
   computed: {
-    lang(){
+    lang() {
       return this.$i18n.locale === "en" ? "en" : "ar";
     },
     services() {
       return this.$i18n.locale === "en" ? ENServices : ARServices;
     },
-    phoneNumber(){
+    phoneNumber() {
       return phoneNumber;
-    }
+    },
   },
   methods: {
-     switchLanguage() {
-      const newLocale = this.$i18n.locale === 'en' ? 'ar' : 'en';
+    switchLanguage() {
+      const newLocale = this.$i18n.locale === "en" ? "ar" : "en";
       this.$i18n.setLocale(newLocale);
     },
     openWhatsApp() {
--- components/OurServicesFields.vue
+++ components/OurServicesFields.vue
@@ -3,7 +3,7 @@
     <v-text-field
       v-model="state.firstName"
       :counter="50"
-      :error-messages="v$.firstName.$errors.map((e) => e.$message)"
+      :error-messages="v$.firstName.$errors.map((e) =>'* ' +  e.$message)"
       :label="$t('firstNameLabel')"
       @blur="v$.firstName.$touch"
       @input="v$.firstName.$touch"
@@ -13,7 +13,7 @@
     <v-text-field
       v-model="state.lastName"
       :counter="50"
-      :error-messages="v$.lastName.$errors.map((e) => e.$message)"
+      :error-messages="v$.lastName.$errors.map((e) =>'* ' +  e.$message)"
       :label="$t('lastNameLabel')"
       @blur="v$.lastName.$touch"
       @input="v$.lastName.$touch"
@@ -23,7 +23,7 @@
     <v-text-field
       v-model="state.email"
       :counter="70"
-      :error-messages="v$.email.$errors.map((e) => e.$message)"
+      :error-messages="v$.email.$errors.map((e) =>'* ' +  e.$message)"
       :label="$t('emailLabel')"
       @blur="v$.email.$touch"
       @input="v$.email.$touch"
@@ -33,7 +33,7 @@
     <v-text-field
       v-model="state.phoneNumber"
       :counter="50"
-      :error-messages="v$.phoneNumber.$errors.map((e) => e.$message)"
+      :error-messages="v$.phoneNumber.$errors.map((e) =>'* ' +  e.$message)"
       :label="$t('phoneLabel')"
       @blur="v$.phoneNumber.$touch"
       @input="v$.phoneNumber.$touch"
@@ -46,25 +46,25 @@
       rows="3"
       auto-grow
       v-model="state.message"
-      :error-messages="v$.message.$errors.map((e) => e.$message)"
+      :error-messages="v$.message.$errors.map((e) => '* ' + e.$message)"
       :label="$t('messageLabel')"
       @blur="v$.message.$touch"
       @change="v$.message.$touch"
       class="filed-style"
     ></v-textarea>
 
-    <div class="d-flex justify-content-center ">
+    <div class="d-flex justify-content-center mt-4">
       <v-btn
         :disabled="loading"
         :loading="loading"
-        class="our-services__submit   rounded-pill p-md-4 p-3 position-relative mt-5 "
+        class="our-services__submit rounded-pill position-relative mt-5"
         color="rgb(81 81 81)"
         type="submit"
       >
-        <span class="title-submit text-light pe-4 text-capitalize"
-          >{{ $t('sendMsg') }}</span
-        >
-           <span class="submit-button btn btn-secondary rounded-pill">
+        <span class="title-submit text-light text-capitalize text-center">
+          {{ $t("sendMsg") }}
+        </span>
+        <span class="submit-button btn btn-secondary">
           <img src="../assets/image/arrow-right.png" alt="" />
         </span>
       </v-btn>
@@ -73,9 +73,11 @@
 </template>
 
 <script setup>
-import { reactive, computed,  ref } from "vue";
+import { reactive, computed, ref } from "vue";
 import { useVuelidate } from "@vuelidate/core";
-import { required, email , numeric , helpers } from "@vuelidate/validators";
+import { required, email, numeric, helpers } from "@vuelidate/validators";
+import axios from "axios";
+
 const i18n = useI18n();
 const initialState = {
   firstName: "",
@@ -85,31 +87,26 @@ const initialState = {
   message: "",
 };
 const state = reactive({ ...initialState });
-const _required = computed(() => helpers.withMessage(() => i18n.t('validations.requiredMsg') , required ));
-const _email = computed(() => helpers.withMessage(() => i18n.t('validations.emailMsg') , email ));
-const _phone = computed(() => helpers.withMessage(() => i18n.t('validations.phone') , numeric ));
+const _required = computed(() =>
+  helpers.withMessage(() => i18n.t("validations.requiredMsg"), required)
+);
+const _email = computed(() =>
+  helpers.withMessage(() => i18n.t("validations.emailMsg"), email)
+);
+const _phone = computed(() =>
+  helpers.withMessage(() => i18n.t("validations.phone"), numeric)
+);
 
 const rules = {
-  firstName: { required : _required.value},
-  lastName: { required:_required.value },
-  email: { required:_required.value, email:_email.value },
-  phoneNumber: { required:_required.value , numeric:_phone.value},
-  message: { required:_required.value },
+  firstName: { required: _required.value },
+  lastName: { required: _required.value },
+  email: { required: _required.value, email: _email.value },
+  phoneNumber: { required: _required.value, numeric: _phone.value },
+  message: { required: _required.value },
 };
 
 const v$ = useVuelidate(rules, state);
 const loading = ref(false);
-async function checkApi(userName) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      if (!userName) return resolve("Please enter a user name.");
-      if (userName === "johnleider")
-        return resolve("User name already taken. Please try another one.");
-
-      return resolve(true);
-    }, 1000);
-  });
-}
 
 async function handleSubmit() {
   const isValid = await v$.value.$validate();
@@ -117,23 +114,29 @@ async function handleSubmit() {
   if (isValid) {
     loading.value = true;
     try {
-      await submitForm();
-      // alert("Form submitted successfully!");
-      clear();
+      await getApi(state);
     } catch (error) {
-      alert(error);
     } finally {
       loading.value = false;
     }
   }
 }
 
-async function submitForm() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 2000);
-  });
+async function getApi(formModel) {
+  await axios
+    .post(`https://admin.digifymena.com/api/v1/message/create`, formModel)
+    .then((response) => {
+      // this.$toast.default("Success, we will contact you soon", {
+      //   position: "bottom-left",
+      // });
+      clear();
+
+    })
+    .catch((error) => {
+      // this.$toast.error("Sorry, something went wrong please try again", {
+      //   position: "bottom-left",
+      // });
+    });
 }
 
 function clear() {
