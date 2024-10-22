26becd0438a3ad3fc043a41c10763f37dfd9ecd5
kkhalil <kkhalil@moj.gov.sa>
2024-10-22 17:47:08 +0300

    review:apply review comments

--- components/SubmitBtn.vue
+++ components/SubmitBtn.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="base-btn">
     <v-btn
       :disabled="loading"
       :loading="loading"
       :class="{ 'active-hover': activeHover }"
-      class="btn__submit rounded-pill position-relative mt-5"
+      class="base-btn base-btn--submit rounded-pill position-relative mt-5"
       color="rgb(81 81 81)"
       type="submit"
       @click="handleClick"
     >
-      <span class="title-submit text-light text-capitalize text-center">
+      <span class="base-btn__title text-light text-capitalize text-center">
         {{ $t("sendMsg") }}
       </span>
-      <span class="submit-button btn btn-secondary">
+      <span class="base-btn__arrow btn btn-secondary">
         <img src="../assets/image/arrow-right.png" alt="" />
       </span>
     </v-btn>
-  </div>
 </template>
 
 <script setup>
