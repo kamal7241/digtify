30b80c9fce38f0cfb3287ff46d64b6e9c9c1c9e2
Mohamed Korney Mohamed <123994927+mohamedkorneymohamed@users.noreply.github.com>
2024-10-22 01:18:20 -0700

    feat: add domain in config google analytic

--- plugins/analytics.js
+++ plugins/analytics.js
@@ -1,14 +1,17 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  const GA_TRACKING_ID = proccess.env.NUXT_PUBLIC_GOOGLE_ANALYTICS_KEY; 
-
+  const GA_TRACKING_ID = proccess.env.NUXT_PUBLIC_GOOGLE_ANALYTICS_KEY;
   if (process.client) {
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       window.dataLayer.push(arguments);
     }
     window.gtag = gtag;
+
     gtag("js", new Date());
-    gtag("config", GA_TRACKING_ID);
+
+    gtag("config", GA_TRACKING_ID, {
+      cookie_domain: process.env.NUXT_DOMAIN, 
+    });
 
     nuxtApp.hook("page:finish", (page) => {
       gtag("config", GA_TRACKING_ID, {
